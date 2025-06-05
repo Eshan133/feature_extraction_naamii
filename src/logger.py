7a16720c779f1e402440e827f3515b47e67d2441
Eshan133 <ishan.maharjan5@gmail.com>
2025-06-05 10:20:44 +0545

    Setting up project

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(), "logs",LOG_FILE) # creating logs folder in current working directory
+os.makedirs(logs_path, exist_ok=True)
+
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# overriding the logging we imported above
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+# Checking if the logger is working as intended  
+# if __name__ == "__main__":
+#     logging.info("Logging has started")
